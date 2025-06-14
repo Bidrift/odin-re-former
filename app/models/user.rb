c7c5654834baa7f8abe89d2e2b433da62d452b5f
Bidrift <yassine.fighter@gmail.com>
2025-06-14 23:53:59 +0100

    Fix typo in validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :username, :email, :password, presence: true
-    validates :email, format { with: URI::MailTo::EMAIL_REGEXP }
-    validates :username, uniqueness: true, length: { in 4..10 }
-    validates :password, length { in 6..16 }
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :username, uniqueness: true, length: { in: 4..10 }
+    validates :password, length: { in: 6..16 }
 end
