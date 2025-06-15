f1b25abcf9582680142aa674a1ed62ae133d448c
Bidrift <yassine.fighter@gmail.com>
2025-06-15 12:57:11 +0100

    Implement update controller method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,13 @@ class UsersController < ApplicationController
     end
 
     def update
+        @user = User.find(params[:id])
 
+        if @user.update(user_params)
+            redirect_to edit_user_path
+        else
+            render :edit, status: :unprocessable_entity
+        end
     end
 
     private
